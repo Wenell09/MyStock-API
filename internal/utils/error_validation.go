e6add51514813728da3cc55dcdd72b456b57942d
Nabil Al Faatir <nabil_alfaatir09@yahoo.com>
2026-02-07 12:53:53 +0700

    fix(utils): change format error validation with validator

--- internal/utils/error_validation.go
+++ internal/utils/error_validation.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/go-playground/validator"
 	"github.com/gofiber/fiber/v2"
 )
 
-// validation manual
 type ValidationError struct {
 	Msg string
 }
@@ -26,43 +28,14 @@ func (v ValidationError) ErrorData() interface{} {
 	return v.Msg
 }
 
-// validation menggunakan validator
-type FieldDetail struct {
-	Field string `json:"field"`
-	Error string `json:"error"`
-}
-
-type FieldError struct {
-	Errors []FieldDetail `json:"errors"`
-}
-
-func (f FieldError) Error() string {
-	return "validation error"
-}
-
-func (f FieldError) StatusCode() int {
-	return fiber.StatusBadRequest
-}
-
-func (f FieldError) ResponseMessage() string {
-	return "Validation Error"
-}
-
-func (f FieldError) ErrorData() interface{} {
-	return f.Errors
-}
-
 func NewFieldError(validationErr validator.ValidationErrors) error {
-	dataErr := []FieldDetail{}
-
+	var messages []string
 	for _, fieldErr := range validationErr {
-		dataErr = append(dataErr, FieldDetail{
-			Field: fieldErr.Field(),
-			Error: fieldErr.Tag(),
-		})
+		messages = append(messages,
+			fmt.Sprintf("%s %s", fieldErr.Field(), fieldErr.Tag()),
+		)
 	}
-
-	return FieldError{
-		Errors: dataErr,
+	return ValidationError{
+		Msg: strings.Join(messages, ", "),
 	}
 }
